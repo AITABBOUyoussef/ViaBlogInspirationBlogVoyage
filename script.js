eb10a3105388ae0765af1798b8f3d9f82bdb3110
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-02-03 15:08:42 +0100

    Add cart element to home.html and initialize it in script.js;

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 
 
+
+// 1. Définition de l'état global
+let voyages = [
+    {
+        id: Date.now(), // ID unique b l-waqt
+        titre: "Ghouroub f Agadir",
+        destination: "Agadir, Maroc",
+        date: "2026-02-01",
+        note: 5,
+        image: "https://images.unsplash.com/photo-1539650116574-8efeb43e2750", // Tswira mn andek
+        categorie: "Plage"
+    },
+    {
+        id: Date.now() + 1,
+        titre: "Randonnée f Ifrane",
+        destination: "Ifrane, Maroc",
+        date: "2026-01-15",
+        note: 4,
+        image: "https://images.unsplash.com/photo-1548625361-125ee292299c",
+        categorie: "Montagne"
+    }
+];
+const cart = document.getElementById('cart');
+
     const btn = document.getElementById('menu-btn');
     const menu = document.getElementById('mobile-menu');
     
